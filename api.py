11476f7896ce73d6b016babddfc7c593d93834f9
yuichu <23323904+yuichu@users.noreply.github.com>
2022-04-28 22:50:56 -0700

    some changes added to figure out fastapi  thinge

--- api.py
+++ api.py
@@ -6,57 +6,58 @@ import logging.config
 import sqlite3
 import re
 
+DATABASE = './var/words.db'
+SCHEMA = './share/words.sql'
 
-#Professor's code
+settings = Settings()
+app = FastAPI()
+
+'''
 class Settings(BaseSettings):
     database: str
     logging_config: str
 
     class Config:
         env_file = ".env"
-
+'''
 class Words(BaseModel):
     wordID: int
     word: str
 
-#db open--------------------------------------------------------
-# this function goes through a master word file,
-#   removes invalid words,
-#   and saves each word in a list
-
-def word_list():
-     listWords = []
-     wordID = 1
-     file = open("/usr/share/dict/words") # opens master list
-     for line in file.readlines():
-        lowerLine = line.lower() # change word in master list to lowercase
-        # currentWord = re.findall(r'\b(\w{6})\b', lowerLinewordList)
-        # eachWord = (wordID , currentWord)
-        # listWords.append(eachWord)
-        # wordID += 1
-        listWords += re.findall(r"\b([a-z]{5}$)\b", lowerLine)
-
-# this function saves the final word list with all invalid words removed
-#   to answers.db database
-def createDBforWords():
-    listWords = word_list()
-    connection = sqlite3.connect("words.db")
-    cursor = connection.cursor()
-    cursor.execute("CREATE TABLE words (word)")
-    for item in listWords:
-        cursor.execute("INSERT INTO words VALUES(?)", (item, )) 
-    connection.commit()  
-    connection.close()
-
+# db open-------------------------------------------------------------
 def get_db():
-    with contextlib.closing(sqlite3.connect('/share/words.db')) as db:
+    with contextlib.closing(sqlite3.connect(DATABASE)) as db:
         db.row_factory = sqlite3.Row
         yield db
+# end db open---------------------------------------------------------
 
-# db open end---------------------------------------------------------------
 
-settings = Settings()
-app = FastAPI()
+# insert words to db--------------------------------------------------
+# this function goes through a master word file,
+#   removes invalid words and saves each word in a list
+# this function saves the word list with all invalid words removed 
+#   to answers.db database
+@app.post("/words/")
+async def createDBforWords(db: sqlite3.Connection = Depends(get_db)):
+    listWords = []
+    file = open("/usr/share/dict/words") # opens master list
+    for line in file.readlines():
+            lowerLine = line.lower() # change word in master list to lowercase
+            listWords += re.findall(r"\b([a-z]{5}$)\b", lowerLine) # fix word and append to list
+    
+    cursor = db.cursor()
+    cursor.execute("CREATE TABLE words (word)")
+    
+    for item in listWords:
+        cursor.execute("INSERT INTO words VALUES(?)", (item, )) 
+    
+    db.commit()  
+    db.close()
+# end insert words to db----------------------------------------------
+
+
+
+
 
 logging.config.fileConfig(settings.logging_config)
 
@@ -81,14 +82,9 @@ class Wotd(BaseModel):
     wordID: int
     word: str
     
-
-
-
 #http://127.0.0.1:8000/docs
 #navigate your browser to http://localhost:8000/words/
 @app.get("/words/{word_ID}")
 async def create_item(word_ID: int, word: Wotd):
     return {"word_ID": word_ID, **word.dict()}
-
-
 '''
